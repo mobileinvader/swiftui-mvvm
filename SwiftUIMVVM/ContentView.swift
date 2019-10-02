311cee5d324836f07397adc4f045393d2a611e67
Kristijan Kralj <kristijan.kralj@live.com>
2019-10-02 06:35:52 +0200

    Initial Commit

---
+++ SwiftUIMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMVVM
+//
+//  Created by Kristijan Kralj on 02/10/2019.
+//  Copyright © 2019 Kristijan Kralj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
